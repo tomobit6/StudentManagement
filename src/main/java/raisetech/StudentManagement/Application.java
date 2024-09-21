d2cac861b4d2665219f6317b4beb777ba5fa6295
tomobit6 <kotenkotenko6@gamil.com>
2024-09-21 14:10:51 +0900

    CRUD処理機能を実装

---
+++ src/main/java/raisetech/StudentManagement/Application.java
@@ -0,0 +1,51 @@
+package raisetech.StudentManagement;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+  @Autowired
+  private StudentRepository repository;
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @GetMapping("/student")
+  public String getStudent(@RequestParam String name) {
+    Student student = repository.searchByName(name);
+    return student.getId() + ":" + student.getName() + " " + student.getAge() + "歳";
+  }
+
+  @GetMapping("/students")
+  public List<Student> getAllStudents() {
+    return repository.findAllStudents();
+  }
+
+  @PostMapping("/student")
+  public void registerStudent(int id, String name, int age) {
+    repository.registerStudent(id, name, age);
+  }
+
+  @PatchMapping("/student")
+  public void updateStudent(int id, String name, int age) {
+    repository.updateStudent(id, name, age);
+  }
+
+  @DeleteMapping("/student")
+  public void deleteStudent(String name) {
+    repository.deleteStudent(name);
+  }
+}
---
+++ src/main/java/raisetech/StudentManagement/StudentRepository.java
@@ -0,0 +1,32 @@
+package raisetech.StudentManagement;
+//データベースを操作するものだと思ったらいい
+
+import java.util.List;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface StudentRepository {
+  //名前から学生情報の取得
+  @Select("SELECT * FROM student WHERE name = #{name}")
+  Student searchByName(String name);
+
+  //全ての学生情報の取得
+  @Select("SELECT * FROM student")
+  List<Student>findAllStudents();
+
+  //学生情報の登録
+  @Insert("INSERT student (name,age) values(#{name},#{age})")
+  void registerStudent(int id,String name,int age);
+
+  //学生情報の更新
+  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
+  void updateStudent(int id,String name,int age);
+
+  //学生情報を削除
+  @Delete("DELETE FROM student WHERE name = #{name}")
+  void deleteStudent(String name);
+}
