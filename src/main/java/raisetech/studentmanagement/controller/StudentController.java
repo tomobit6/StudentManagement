bcb07da7dbd70837fc2d2d8663611a7c7a9f77e1
tomobit6 <kotenkotenko6@gamil.com>
2024-10-09 20:27:58 +0900

    意図していない差分が生まれたためコミット

--- src/main/java/raisetech/studentmanagement/controller/StudentController.java
+++ src/main/java/raisetech/studentmanagement/controller/StudentController.java
@@ -2,15 +2,15 @@ package raisetech.studentmanagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.studentmanagement.controller.converter.StudentConverter;
 import raisetech.studentmanagement.data.Student;
 import raisetech.studentmanagement.data.StudentCourses;
-import raisetech.studentmanagement.domain.StudentDetail;
 import raisetech.studentmanagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
 
@@ -24,12 +24,11 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentCourses> studentCourses = service.searchStudentCoursesList();
-
-    return converter.convertStudentDetails(
-        students, studentCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
+    return "studentList";
   }
 
   @GetMapping("/studentCourseList")
