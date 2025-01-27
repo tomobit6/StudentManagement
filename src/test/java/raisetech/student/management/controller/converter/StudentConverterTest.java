7ce0839e9f160e3bc89cbce86b2fece14ebc047f
tomobit6 <kotenkotenko6@gamil.com>
2025-01-27 19:32:36 +0900

    Converterクラスの不要なモック削除

--- src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
+++ src/test/java/raisetech/student/management/controller/converter/StudentConverterTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentCourse;
 import raisetech.student.management.domain.StudentDetail;
@@ -16,13 +15,16 @@ class StudentConverterTest {
 
   @Test
   void 正常系_受講生や受講生コース情報が正しく受講生詳細に変換できていること() {
-    Student student1 = Mockito.mock(Student.class);
-    Mockito.when(student1.getId()).thenReturn("777");
-    Student student2 = Mockito.mock(Student.class);
-    Mockito.when(student2.getId()).thenReturn("888");
-    Student student3 = Mockito.mock(Student.class);
-    Mockito.when(student3.getId()).thenReturn("999");
-    
+    Student student1 = new Student("777", "仮名前", "かりなまえ", "カリ", "karinamae@example.com",
+        "高知県高知市",
+        77, "男", "", false);
+    Student student2 = new Student("888", "偽名前", "にせなまえ", "ニセ", "nisenamae@example.com",
+        "高知県高知市",
+        88, "女", "", false);
+    Student student3 = new Student("999", "疑似名前", "ぎじなまえ", "ギジ", "gizinamae@example.com",
+        "高知県高知市",
+        99, "その他", "", false);
+
     List<Student> studentList = List.of(student1, student2, student3);
 
     StudentCourse studentCourse1 = new StudentCourse(student1.getId(), "Javaコース");
